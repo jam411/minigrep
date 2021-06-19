6c38f33c64bc6dba57ebff561aa1c49f75b03f29
Ryoji Simizu <ryojisimizu@wl206167.jaist.ac.jp>
2021-06-19 21:34:34 +0900

    Creating a failing test for the search function we wish we had

---
+++ target/debug/deps/minigrep-f37041d7991e56a0.d
@@ -0,0 +1,5 @@
+/Users/ryojisimizu/rust/minigrep/target/debug/deps/minigrep-f37041d7991e56a0: src/main.rs
+
+/Users/ryojisimizu/rust/minigrep/target/debug/deps/minigrep-f37041d7991e56a0.d: src/main.rs
+
+src/main.rs:
