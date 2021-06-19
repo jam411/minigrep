1261b749e47d197228d7cd70199ddde12ce275a8
Ryoji Simizu <ryojisimizu@wl206167.jaist.ac.jp>
2021-06-19 21:56:44 +0900

    fixing some typo's

--- src/lib.rs
+++ src/lib.rs
@@ -45,12 +45,12 @@ pub fn search_case_insensitive<'a>(
     query: &str,
     contents: &'a str,
 ) -> Vec<&'a str> {
-    let query = query.to_lowercase()();
+    let query = query.to_lowercase();
     let mut results = Vec::new();
 
     for line in contents.lines() {
-        if line.to_lowercase().contains((&query) {
-            results.push(liine);
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
         }
     }
 
